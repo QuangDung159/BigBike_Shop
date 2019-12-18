4508bec5feb4f9dabf69928d94130bce3387b4ff
DungLu <quangdunglu159@gmail.com>
2019-12-18 17:51:16 +0700

    Add

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -119,7 +119,6 @@ class DatabaseSeeder extends Seeder
         $this->call(CategorySeeder::class);
         $this->call(BrandSeeder::class);
         $this->call(BrandCategorySeeder::class);
-
         $this->call(ProductSeeder::class);
     }
 }
