f15063a2d0b8276915e323a68ff65cafbecbe40b
DungLu <quangdunglu159@gmail.com>
2019-12-24 18:01:22 +0700

    brand category read, create

---
+++ resources/views/admin/page/brand_category/edit.blade.php
@@ -0,0 +1,76 @@
+@extends('admin.layout.master')
+@section('content')
+    <section id="main-content">
+        <section class="wrapper">
+            <div class="form-w3layouts">
+                <!-- page start-->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <section class="panel">
+                            <header class="panel-heading">
+                                Edit Category {{$category->category_name}}
+                                <span class="tools pull-right">
+{{--                                <a class="fa fa-chevron-down" href="javascript:;"></a>--}}
+                                    {{--                                <a class="fa fa-cog" href="javascript:;"></a>--}}
+                                    {{--                                <a class="fa fa-times" href="javascript:;"></a>--}}
+                             </span>
+                            </header>
+                            <div class="panel-body">
+                                <form role="form" class="form-horizontal" action="{{URL::to('/admin/category/update')}}"
+                                      method="post"
+                                      enctype="multipart/form-data">
+                                    {{csrf_field()}}
+                                    <input type="hidden" name="category_id" value="{{$category->category_id}}">
+                                    <div class="form-group">
+                                        <label class="col-lg-3 control-label">Category Name</label>
+                                        <div class="col-lg-6">
+                                            <input type="text" placeholder="Enter category name" name="category_name"
+                                                   id="f-name" value="{{$category->category_name}}"
+                                                   class="form-control">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-lg-3 control-label">Category Description</label>
+                                        <div class="col-lg-6">
+                                            <textarea type="text" placeholder="Enter category description"
+                                                      name="category_description"
+                                                      id="l-name"
+                                                      class="form-control">{{$category->category_desc}}</textarea>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-lg-3 control-label"></label>
+                                        <div class="col-lg-6">
+                                            @if($errors->any())
+                                                <div class="alert alert-danger" style="margin-top: 10px">
+                                                    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                                                    @foreach($errors->all() as $errorItem)
+                                                        <p>{{$errorItem}}</p>
+                                                    @endforeach
+                                                </div>
+                                            @endif
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <div class="col-lg-offset-3 col-lg-6">
+                                            <button class="btn btn-primary" type="submit">Submit</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+                <!-- page end-->
+            </div>
+        </section>
+        <!-- footer -->
+        <div class="footer">
+            <div class="wthree-copyright">
+                <p>© 2017 Visitors. All rights reserved | Design by <a href="http://w3layouts.com">W3layouts</a></p>
+            </div>
+        </div>
+        <!-- / footer -->
+    </section>
+@endsection
